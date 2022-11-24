a8480c53bd4d09680c73767073a2e604ad662754
eumoon <euromoon@protonmail.com>
2022-11-24 19:29:01 -0300

    Início do desenvolvimento da UI.

--- Scripts/Background.cs
+++ Scripts/Background.cs
@@ -3,42 +3,42 @@ using System;
 
 public class Background : Sprite
 {
-    // Declare member variables here. Examples:
-    private int currentLine = 0;
+	// Declare member variables here. Examples:
+	private int currentLine = 0;
 
-    private void GenerateLine() {
-        var line = new Line2D();
-        line.DefaultColor = Colors.White;
-        line.ZAsRelative = false;
-        
-        line.ZIndex = 10;
-        if (currentLine == 0 || currentLine == 15 || currentLine == 16 || currentLine == 31) {
-            line.Width = 4;
-        } else {
-            line.Width = 2f;
-        }
+	private void GenerateLine() {
+		var line = new Line2D();
+		line.DefaultColor = Colors.White;
+		line.ZAsRelative = false;
+		
+		line.ZIndex = 10;
+		if (currentLine == 0 || currentLine == 15 || currentLine == 16) {
+			line.Width = 4;
+		} else {
+			line.Width = 2f;
+		}
 
-        if (currentLine <= 15) {
-            line.AddPoint(new Vector2(currentLine * 40, 0));
-            line.AddPoint(new Vector2(currentLine * 40, 600));
-        } else {
-            line.AddPoint(new Vector2(0, (currentLine - 16) * 40));
-            line.AddPoint(new Vector2(1024, (currentLine - 16) * 40));
-        }
+		if (currentLine <= 15) {
+			line.AddPoint(new Vector2(currentLine * 40, 0));
+			line.AddPoint(new Vector2(currentLine * 40, 600));
+		} else {
+			line.AddPoint(new Vector2(0, (currentLine - 16) * 40));
+			line.AddPoint(new Vector2(1024, (currentLine - 16) * 40));
+		}
 
-        AddChild(line);
+		AddChild(line);
 
-        currentLine++;
-    }
+		currentLine++;
+	}
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        for (int i = 0; i < 32; i++)
-        {
-            GenerateLine();
-        }
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		for (int i = 0; i < 32; i++)
+		{
+			GenerateLine();
+		}
+	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
